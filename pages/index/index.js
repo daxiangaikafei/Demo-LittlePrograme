91f1e77ec03e7b8d683b7562d6ad809faaa071a6
FIRST_NAME LAST_NAME <mail@xuechen.wang>
2017-06-01 14:20:01 +0800

    add files

---
+++ pages/index/index.js
@@ -0,0 +1,58 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'WeChat Demo',
+    title: 'Just for WeChat Demo',
+    userInfo: {},
+    list: [],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      console.log(userInfo)
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+    app.getMusicList(function(list){
+      that.setData({
+        list: list
+      })
+    });
+  }
+})
